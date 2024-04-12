8d9dbcab48c7429449a43f43b12b38d1b4cf9128
parsa <parsagorgzan1225@gmail.com>
2024-04-12 20:52:53 +0330

    Root Urls

--- MySite/MySite/urls.py
+++ MySite/MySite/urls.py
@@ -22,6 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('', include('blog.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
