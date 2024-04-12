4a3577abb3cc5ef20360fc88dd92ec3d944c6187
parsa <parsagorgzan1225@gmail.com>
2024-04-12 19:57:42 +0330

    UserAdmin

--- MySite/blog/admin.py
+++ MySite/blog/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
-from .models import Post
+from django.db.models import Count
+from . import models
 # Register your models here.
 
-admin.site.register(Post)
+
+@admin.register(models.ProfileUser)
+class ProfileUserAdmin(admin.ModelAdmin):
+    list_display = ['get_username', 'post_count', 'phone_number']
+    search_fields = ['get_username']
+    list_per_page = 10
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(post_count=Count('post'))
+
+    @admin.display(ordering='post_count')
+    def post_count(self, obj):
+        return obj.post_count
