faa87acb50d4acd1ddd7e78029c25ffbc8360599
parsa <parsagorgzan1225@gmail.com>
2024-04-25 19:26:19 +0330

    add image to Post model

--- MySite/blog/models.py
+++ MySite/blog/models.py
@@ -60,6 +60,7 @@ class Post(models.Model):
     root_post = models.ForeignKey('Post', on_delete=models.CASCADE, null=True, blank=True)
     author = models.ForeignKey(ProfileUser, on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tag)
+    image = models.ImageField(upload_to='blog/media/images', null=True, blank=True)
 
     def __str__(self):
         return self.title
