2d24cb43cd9440a815034d2d63a697eeb8faaf10
parsa <parsagorgzan1225@gmail.com>
2024-04-26 06:24:31 +0330

    Media root

--- MySite/MySite/settings.py
+++ MySite/MySite/settings.py
@@ -147,6 +147,9 @@ STATICFILES_DIRS = (
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+MEDIA_URL = '/media/'  # URL to access media files
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # Directory to store media files
+
 
 CKEDITOR_BASEPATH = "/staticfiles/ckeditor/ckeditor"
 # Default primary key field type
