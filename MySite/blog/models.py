372504fa819b14c933f189cfef334f30c19286c3
parsa <parsagorgzan1225@gmail.com>
2024-05-03 09:04:28 +0330

    Comment models

--- MySite/blog/models.py
+++ MySite/blog/models.py
@@ -81,10 +81,23 @@ class Post(models.Model):
     title = models.CharField(max_length=25)
     content = RichTextField()
     pub_date = models.DateTimeField(auto_now_add=True)
-    root_post = models.ForeignKey('Post', on_delete=models.CASCADE, null=True, blank=True)
+    root_post = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
     author = models.ForeignKey(ProfileUser, on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tag)
     image = models.ImageField(upload_to='blog/media/images', null=True, blank=True)
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    parent = models.ForeignKey(
+        'self', on_delete=models.CASCADE, null=True, blank=True, related_name='replies'
+    )
+
+    def __str__(self):
+        return f'Comment by {self.author} on {self.post.title}'
